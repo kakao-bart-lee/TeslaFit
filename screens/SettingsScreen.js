a7155b287ea4207b4ef051146daed4c691efc4fc
bart.lee <bart.lee@kakaocorp.com>
2020-01-19 20:48:11 +0900

    tryyyy

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import {ExpoConfigView} from '@expo/samples';
+import Settings from "../mycomponent/SettingsClass";
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+  return <Settings />;
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  header: null,
 };
+
